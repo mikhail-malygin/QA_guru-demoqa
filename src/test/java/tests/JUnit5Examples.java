514b55a580bafa62be683eb5a37000f4aa0927ef
Mikhail Malygin <mihail.malygin@gmail.com>
2022-05-29 16:50:44 +0500

    added JUnit5Examples

---
+++ src/test/java/tests/JUnit5Examples.java
@@ -0,0 +1,41 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+
+public class JUnit5Examples {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("== Something before all tests");
+    }
+
+    @BeforeAll
+    static void beforeAll1() {
+        System.out.println("== Something 1 before all tests");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("==== Something before every tests");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("======== Started first test");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("======== Started second test");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("================ Something after every tests");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("================================ Something after all tests");
+    }
+}
