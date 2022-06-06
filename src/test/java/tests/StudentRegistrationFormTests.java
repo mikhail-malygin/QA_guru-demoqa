01842afbd9c8f351ef2539f8c5b5f4ab91da137d
Mikhail Malygin <mihail.malygin@gmail.com>
2022-06-06 12:04:32 +0500

    Refactoring. Added TestData

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -2,51 +2,38 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import static tests.TestData.*;
+
+
 public class StudentRegistrationFormTests extends TestBase {
 
     @Test
     void fillingRegistrationFormTest() {
 
-        String firstName = "Mikhail";
-        String lastName = "Malygin";
-        String userEmail = "test@mail.ru";
-        String gender = "Male";
-        String phoneNumber = "1234567890";
-        String dayOfBirth = "30";
-        String monthOfBirth = "May";
-        String yearOfBirth = "2022";
-        String subject = "Computer Science";
-        String hobby = "Reading";
-        String pathPicture = "img/test_picture_qa_guru.png";
-        String namePicture = "test_picture_qa_guru.png";
-        String currentAddress = "Moscow, 17 Tverskaya st";
-        String state = "Haryana";
-        String city = "Panipat";
-
         studentRegistrationFormPage.openPage()
-                                   .setFirstName(firstName)
-                                   .setLastName(lastName)
-                                   .setUserEmail(userEmail)
-                                   .setGender(gender)
-                                   .setPhoneNumber(phoneNumber)
-                                   .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
-                                   .setSubject(subject)
-                                   .setHobby(hobby)
-                                   .uploadPicture(pathPicture)
-                                   .setCurrentAddress(currentAddress)
-                                   .setState(state)
-                                   .setCity(city)
+                                   .setFirstName(FIRST_NAME)
+                                   .setLastName(LAST_NAME)
+                                   .setUserEmail(USER_EMAIL)
+                                   .setGender(GENDER)
+                                   .setPhoneNumber(PHONE_NUMBER)
+                                   .setDateOfBirth(DAY_OF_BIRTH, MONTH_OF_BIRTH, YEAR_OF_BIRTH)
+                                   .setSubject(SUBJECT)
+                                   .setHobby(HOBBY)
+                                   .uploadPicture(PATH_PICTURE)
+                                   .setCurrentAddress(CURRENT_ADDRESS)
+                                   .setState(STATE)
+                                   .setCity(CITY)
                                    .submit();
 
-        studentRegistrationFormPage.checkResult("Student Name", firstName + " " + lastName)
-                                   .checkResult("Student Email", userEmail)
-                                   .checkResult("Gender", gender)
-                                   .checkResult("Mobile", phoneNumber)
-                                   .checkResult("Date of Birth", (dayOfBirth + " " + monthOfBirth + "," + yearOfBirth))
-                                   .checkResult("Subjects", subject)
-                                   .checkResult("Hobbies", hobby)
-                                   .checkResult("Picture", namePicture)
-                                   .checkResult("Address", currentAddress)
-                                   .checkResult("State and City", state + " " + city);
+        studentRegistrationFormPage.checkResult("Student Name", FIRST_NAME + " " + LAST_NAME)
+                                   .checkResult("Student Email", USER_EMAIL)
+                                   .checkResult("Gender", GENDER)
+                                   .checkResult("Mobile", PHONE_NUMBER)
+                                   .checkResult("Date of Birth", (DAY_OF_BIRTH + " " + MONTH_OF_BIRTH + "," + YEAR_OF_BIRTH))
+                                   .checkResult("Subjects", SUBJECT)
+                                   .checkResult("Hobbies", HOBBY)
+                                   .checkResult("Picture", NAME_PICTURE)
+                                   .checkResult("Address", CURRENT_ADDRESS)
+                                   .checkResult("State and City", STATE + " " + CITY);
     }
 }
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,19 @@
+package tests;
+
+public class TestData {
+    public final static String FIRST_NAME = "Mikhail",
+                               LAST_NAME = "Malygin",
+                               USER_EMAIL = "test@mail.ru",
+                               GENDER = "Male",
+                               PHONE_NUMBER = "1234567890",
+                               DAY_OF_BIRTH = "30",
+                               MONTH_OF_BIRTH = "May",
+                               YEAR_OF_BIRTH = "2022",
+                               SUBJECT = "Computer Science",
+                               HOBBY = "Reading",
+                               PATH_PICTURE = "img/test_picture_qa_guru.png",
+                               NAME_PICTURE = "test_picture_qa_guru.png",
+                               CURRENT_ADDRESS = "Moscow, 17 Tverskaya st",
+                               STATE = "Haryana",
+                               CITY = "Panipat";
+}
