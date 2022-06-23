e77ab6bbb6dc0e626334b6c6d30ae3fee6bc032f
Mikhail Malygin <mihail.malygin@gmail.com>
2022-06-23 10:29:09 +0500

    added steps in StudentRegistrationFormTests

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -1,6 +1,8 @@
 package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import pages.demoqa.StudentRegistrationFormPage;
 
@@ -10,6 +12,8 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
